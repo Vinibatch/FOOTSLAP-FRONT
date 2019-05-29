1766a0d21f09c02406512134db569037cfbe9d0f
maxlefevre <maxlefevre@me.com>
2019-05-29 11:23:17 +0200

    new header folder  with ad banner component

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import LiveGameScreen from './components/screens/LiveGameScreen';
 
 import Footer from './components/footer/footer';
+import AdBanner from './components/header/adBanner';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <LiveGameScreen/>
       </View>
     );
   }
---
+++ components/header/adBanner.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, View, Image, ImageBackground } from 'react-native';
+import { Divider } from 'react-native-elements';
+
+export default class AdBanner extends React.Component {
+
+  render() {
+
+      return (
+
+        <View>
+            <Divider style={styles.divide}/>
+            <ImageBackground source={require("../../assets/backgrounds/FootSlap_Bg.png")}
+                style={styles.header}>
+                <Image
+                source={require("../../assets/logos/FSlogo.png")}
+                style={styles.logo}
+                />
+            </ImageBackground>
+        </View>
+      ) 
+    }
+  }
+
+const styles = StyleSheet.create({
+  header: {
+    height: 50,
+    width: '100%',
+  },
+  divide: {
+      height: '15%',
+      backgroundColor: '#00B1FF',
+      width:"100%"
+  },
+    logo: {
+      width: 50, 
+      height: 50, 
+      alignItems: 'center', 
+      alignSelf: 'center'
+    }
+});
