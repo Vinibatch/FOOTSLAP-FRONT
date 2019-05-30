fde8bfea70a2434f5b723c0fe62b8f4bbf82798f
Toooshine <toooshine@gmail.com>
2019-05-30 09:23:04 +0200

    mise en place du changement d'equipe par le biais des boutons sur TeamScreen

--- App.js
+++ App.js
@@ -3,22 +3,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import LiveGameScreen from './components/screens/LiveGameScreen';
 
-import Footer from './components/footer/footer';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <LiveGameScreen/>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+			
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    
-  },
-
+	container: {
+		flex: 1
+	}
 });
