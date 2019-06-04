c076f50b36c997fe5e13c56b2aa016ee8b9c2372
Mika312 <michaelpeltier31284@gmail.com>
2019-06-04 17:27:50 +0200

    start redux

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-
 import Navigation from './components/navigation/navigation';
+import user from './components/reducers/live.reducer';
+import {Provider, connect} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+const store = createStore(combineReducers({user}));
 
-export default class App extends React.Component {
+
+ class App extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 
+	componentDidMount(){
+
+		fetch('http://IpConfig:3000/nom de la route ')
+	.then((response)=>{
+		return response.json();
+	})
+	.then((user)=> {
+		console.log("après affichage",(user));
+		this.props.handleUserFromDB(user)
+	})
+	.catch((error) =>{
+		console.log('Request failed', error)
+	});
+	
+	 }
+	
+
+
 	render() {
 		return (
+			<Provider store={store}>
 			<View style={styles.container}>
 				<Navigation />
 			</View>
+			</Provider>
 		);
 	}
 }
@@ -22,3 +46,20 @@ const styles = StyleSheet.create({
 		flex: 1
 	}
 });
+
+function mapDispatchToProps(dispatch) {
+	return {
+		handleUserFromDB: function(user) {
+	dispatch( {
+	  type: 'getUserFromDB', 
+	  user
+		}) 
+	  }
+	 }
+	}
+	
+	export default connect(
+	null,
+	mapDispatchToProps
+	)(App);
+  
